88c72198c9b8b8f667a6ef68dbc143c2525a8cb4
Georgia <georgia@cranbrookw4.com>
2020-08-26 09:28:41 +0100

    Create golden master test

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,15 @@
 const {Shop, Item} = require("../src/gilded_rose");
+const runOutput = require('./texttest_fixture')
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
-  });
+  // it("should foo", function() {
+  //   const gildedRose = new Shop([new Item("foo", 0, 0)]);
+  //   const items = gildedRose.updateQuality();
+  //   expect(items[0].name).toBe("fixme");
+  // });
+
+  it('Golden master', () => {
+    const output = runOutput();
+    expect(output).toMatchSnapshot()
+  })
 });
