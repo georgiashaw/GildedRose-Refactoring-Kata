40e2614b96dd48f197d59dab3adb78c395d1f88d
Georgia <georgia@cranbrookw4.com>
2020-09-07 10:00:33 +0100

    Psssing tests

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -21,7 +21,6 @@ class Shop {
 
   isBackStagePass(itemName) {
     return itemName == 'Backstage passes to a TAFKAL80ETC concert'
-
   }
 
   qualityGreaterThanZero(itemQuality) {
@@ -36,7 +35,42 @@ class Shop {
     return itemQuality < 50;
   }
 
+sellInlessThanEleven (itemSellIn) {
+return itemSellIn < 11;
+}
+
+sellInLessThanSix (itemSellIn) {
+return itemSellIn < 6;
+}
+
+incrementByOne(itemQuality) {
+  return  itemQuality = itemQuality + 1;
+}
+
+decrementQualityByOne(itemQuality) {
+ return itemQuality - 1;
+}
+
 updateQuality() {
+
+  const handleQualityInBrieAndBackstagePass = (i) => {
+    if (this.qualityLessThanFifty(this.items[i].quality)) {
+      this.items[i].quality = this.items[i].quality + 1;
+      if (this.isBackStagePass(this.items[i].name)) {
+        if (this.sellInlessThanEleven(this.items[i].sellIn)) {
+          if (this.qualityLessThanFifty(this.items[i].quality)) {
+            this.incrementByOne(this.items[i].quality)
+          }
+        }
+        if (this.sellInLessThanSix(this.items[i].sellIn)) {
+          if (this.qualityLessThanFifty(this.items[i].quality)) {
+            this.incrementByOne(this.items[i].quality)
+          }
+        }
+      }
+    }
+  }
+
     for (let i = 0; i < this.items.length; i++) {
       if (this.notAgedBrie(this.items[i].name) && this.notBackstagePass(this.items[i].name)) {
         if (this.qualityGreaterThanZero(this.items[i].quality)) {
@@ -45,21 +79,7 @@ updateQuality() {
           }
         }
       } else {
-        if (this.qualityLessThanFifty(this.items[i].quality)) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.isBackStagePass(this.items[i].name)) {
-            if (this.items[i].sellIn < 11) {
-              if (this.qualityLessThanFifty(this.items[i].quality)) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.qualityLessThanFifty(this.items[i].quality)) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
+        handleQualityInBrieAndBackstagePass(i)
       }
       if (this.notSulfuras(this.items[i].name)) {
         this.items[i].sellIn = this.items[i].sellIn - 1;
@@ -69,7 +89,7 @@ updateQuality() {
           if (this.notBackstagePass(this.items[i].name)) {
             if (this.items[i].quality > 0) {
               if (this.notSulfuras(this.items[i].name)) {
-                this.items[i].quality = this.items[i].quality - 1;
+                this.items[i].quality = decrementQualityByOne(this.items[i].quality);
               }
             }
           } else {
@@ -77,7 +97,7 @@ updateQuality() {
           }
         } else {
           if (this.qualityLessThanFifty(this.items[i].quality)) {
-            this.items[i].quality = this.items[i].quality + 1;
+            this.incrementByOne(this.items[i].quality)
           }
         }
       }
